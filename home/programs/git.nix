4713f6a9676bf4eb06ffa0f509d7573beb48b6a6
= <=>
2024-06-23 18:36:49 -0400

    adds: git ssh signing?

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -4,5 +4,12 @@
 
     userName = "John Reed";
     userEmail = "ReallyJohnReed+git@gmail.com";
+
+    signing.key = "/home/johndr/.ssh/id_ed25519.pub";
+    signing.signByDefault = true;
+
+    extraConfig = {
+      gpg.format = "ssh";
+    };
   };
 }
