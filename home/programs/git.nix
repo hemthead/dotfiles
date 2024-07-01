8f6fa56fde1a27079fc4268087ff7044f8eeaf4d
= <=>
2024-07-01 14:45:49 -0400

    changes git email to all lowercase (might help with github verified commits)

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -3,7 +3,7 @@
     enable = true;
 
     userName = "John Reed";
-    userEmail = "ReallyJohnReed+git@gmail.com";
+    userEmail = "reallyjohnreed+git@gmail.com";
 
     signing.key = "/home/johndr/.ssh/id_ed25519.pub";
     signing.signByDefault = true;
