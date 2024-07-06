a9c961c6cb5b12d958ff74d500e47c738ac0e2ab
John Reed <reallyjohnreed+git@gmail.com>
2024-07-06 19:41:59 -0400

    adds better battery management and cpu throttling(?) for nixtop

--- hosts/nixtop/default.nix
+++ hosts/nixtop/default.nix
@@ -17,6 +17,11 @@
 
   networking.hostName = "nixtop"; # Define your hostname.
 
+  # laptop power management stuff
+  powerManagement.enable = true;
+  services.thermald.enable = true;
+  services.tlp.enable = true; # eventually may be replaced with auto-cpufreq
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
