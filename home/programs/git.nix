81d5ebbf52db6acc56e1b09d3007fc549fefc761
John Reed <reallyjohnreed+git@gmail.com>
2024-07-06 20:11:34 -0400

    adds author to git commits

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -3,11 +3,15 @@
     enable = true;
 
     userName = "John Reed";
+    extraConfig.author.name = "John Reed";
     userEmail = "reallyjohnreed+git@gmail.com";
 
     signing.key = "/home/johndr/.ssh/id_ed25519.pub";
     signing.signByDefault = true;
+    extraConfig.gpg.format = "ssh";
 
-    extraConfig = { gpg.format = "ssh"; };
+    extraConfig.init.defaultBranch = "main";
+
+    extraConfig.url."https://github.com/".insteadOf = [ "gh:" "github:" ];
   };
 }
