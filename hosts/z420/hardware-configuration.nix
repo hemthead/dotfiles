bdc286e587e1bad664e853036d453a81e88e83db
John Reed <reallyjohnreed+git@gmail.com>
2024-08-28 19:43:11 -0400

    fix hibernate for z420

--- hosts/z420/hardware-configuration.nix
+++ hosts/z420/hardware-configuration.nix
@@ -43,7 +43,7 @@
     size = 16*1024;
   }];
   boot.resumeDevice = fileSystems."/".device;
-  boot.kernelParams = [ "mem_sleep_default=deep" "resume_offset=3076096" ];
+  boot.kernelParams = [ "mem_sleep_default=deep" "resume_offset=27381760" ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
