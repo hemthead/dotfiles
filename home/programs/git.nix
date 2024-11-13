33716445409611644f93702c90c3d66587f89f9d
John Reed <reallyjohnreed+git@gmail.com>
2024-11-12 20:03:58 -0500

    remove +git from git email

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -4,7 +4,7 @@
 
     userName = "John Reed";
     extraConfig.author.name = "John Reed";
-    userEmail = "reallyjohnreed+git@gmail.com";
+    userEmail = "reallyjohnreed@gmail.com";
 
     signing.key = "/home/johndr/.ssh/id_ed25519.pub";
     signing.signByDefault = true;
