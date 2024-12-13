e7587f792d8ef54c800e9bedd6b5984990bb6cb8
John Reed <reallyjohnreed@gmail.com>
2024-12-12 22:49:19 -0500

    fix resume_offset kernel param for hibernate on z420

--- hosts/z420/hardware-configuration.nix
+++ hosts/z420/hardware-configuration.nix
@@ -44,7 +44,7 @@
     size = 16 * 1024;
   }];
   boot.resumeDevice = fileSystems."/".device;
-  boot.kernelParams = [ "intel_iommu=on" "mem_sleep_default=deep" "resume_offset=27381760" ];
+  boot.kernelParams = [ "intel_iommu=on" "mem_sleep_default=deep" "resume_offset=30101504" ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
