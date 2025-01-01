905f16436c5b79a04fa85004a307d492838c783d
John Reed <reallyjohnreed@gmail.com>
2025-01-01 16:47:48 -0500

    fix an issue with newer rust-analyzer releases where neovim would be interrupted (carries over into other LSPs

--- home/programs/neovim/extraConfig.lua
+++ home/programs/neovim/extraConfig.lua
@@ -73,3 +73,14 @@ cmp.setup.cmdline(':', {
   }),
   matching = { disallow_symbol_nonprefix_matching = false },
 })
+
+-- Special thanks to @Jesse-Bakker for this fix (https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525)
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
