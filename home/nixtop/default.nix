1ab0158be29ee4a32432e3f465a8b73e13b52339
John Reed <reallyjohnreed@gmail.com>
2025-07-17 14:39:42 -0400

    swap capslock and escape on nixtop

--- home/nixtop/default.nix
+++ home/nixtop/default.nix
@@ -1,1 +1,5 @@
-{ imports = [ ../default.nix ./swayidle.nix ]; }
+{
+  imports = [ ../default.nix ./swayidle.nix ];
+  wayland.windowManager.sway.extraConfig =
+    ''input * xkb_options "caps:swapescape"'';
+}
